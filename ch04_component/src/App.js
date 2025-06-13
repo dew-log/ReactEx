0800840355920d8d5beed6cf8b5cd174b6bd9a1a
dew <foaming21@gmail.com>
2025-06-13 17:11:15 +0900

    비구조 할당과 children 요소

--- ch04_component/src/App.js
+++ ch04_component/src/App.js
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+
 // import Child from './ex01_function/Child';
 // import Child from './ex02_class/Child';
 // import MyComponent from "./ex03_props/MyComponent";
-import Library from "./ex04_props/Library";
+// import Library from "./ex04_props/Library";
+// import MyComponent from "./ex05_arrow/MyComponent";
+import MyComponent from "./ex06_destructing/MyComponent";
 
 function App() {
   return (
@@ -17,9 +20,24 @@ function App() {
     </>
     */
 
+    /*
     <>
       <Library title='유학' />
       <Library title='조선 선비들의' />
+    </>
+    */
+
+    /*
+    <>
+      <MyComponent name="python" />
+      <MyComponent name="javascript" />
+      <MyComponent name="java" />
+    </>
+    */
+
+    <>
+      <MyComponent name='리액트'>React Programming</MyComponent>
+      <MyComponent name='React, Vue'>컴포넌트 제작</MyComponent>
     </>
   );
 }
