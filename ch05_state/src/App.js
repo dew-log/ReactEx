4c197e80f34263c4fb51f132aa727e24cc9a7cbe
dew <foaming21@gmail.com>
2025-06-16 12:19:46 +0900

    useState

---
+++ ch05_state/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import Counter from './ex01_state/Counter';
+// import Say from './ex02_state/Say';
+import Say from './ex03_multistate/Say';
+
+function App() {
+  return (
+    // <Counter/>
+
+    <Say/>
+  );
+}
+
+export default App;
---
+++ ch05_state/src/ex01_state/Counter.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+/*
+; (마치 갈고리를 걸은 것처럼 변경사항을 모니터링)
+ state
+
+ 1) 지역변수 number를 쓸 때의 문제점
+    ; Counter 함수 내에 선언하게 되면
+      Counter가 다시 그려질 때(Rendering)
+      같이 초기화 된다는 단점이 있다.
+
+ 2) 전역변수 number를 쓸 때의 문제점
+    ; Counter 함수 외부의 변수와 함수가 연결 지어지므로
+      독립성이 훼손되며, 전역변수가 많아지면 관리가 어렵다.
+      그리고 전역변수와 함수를 항상 함께 묶어야 한다.
+
+3) 위의 2가지에 대한 대안으로 state를 만들었다.
+    number의 값을 직접 변경하지 말고,
+    반드시 setNumber를 통해 변경해야 한다.
+    그래야 React의 Hook 매커니즘이 동작해서
+    state가 포함된 함수 컴포넌트의 ReRendering이 발생한다
+    그리고 state는 React에 의해서 값이 보관되므로
+    ReRendring과 관계없이 값이 유지된다
+*/
+
+function Counter(){
+    // state 변수, state 함수  = number의 초기값
+    const {number, setNumber} = useState;
+
+return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={() => {
+          setNumber(number + 1);
+          /* setNumber의 역할
+          1) number = number-1
+          2) React엔진에 Counter의 ReRendring 이벤트를 전달
+          */
+        }}> ++ </button>
+    </div>
+  );
+}
+
+export default Counter;
