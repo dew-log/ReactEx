88abed8784d45cf156f1c575bb9f503227c31092
dew <foaming21@gmail.com>
2025-06-17 09:36:48 +0900

    event

---
+++ ch06_event/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+// import Say from './ex02_html_event/Say';
+import Say from './ex03_event/EventPractice';
+
+function App() {
+  return (
+    <Say/>
+  );
+}
+
+export default App;
