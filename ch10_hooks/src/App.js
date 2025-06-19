cad4ffe3d4ad55a85c17251cf15146f5bd2f0560
dew <foaming21@gmail.com>
2025-06-19 16:21:24 +0900

    useEffect 구현

---
+++ ch10_hooks/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import Counter from './ch01_useState/Counter';
+// import Info from './ch02_useState/Info';
+// import Info from './ch03_useEffect/Info';
+// import Info from './ch04_useEffect/Info';
+// import Info from './ch05_useEffect/Info';
+// import Info from './ch06_useEffect/Info';
+import Info from './ch07_useEffect/Info';
+
+function App() {
+  return (
+    // <Counter />
+
+    <Info />
+  );
+}
+
+export default App;
