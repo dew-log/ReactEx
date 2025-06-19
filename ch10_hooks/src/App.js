45b93aca72c3296809715306c92c7a669616d495
dew <foaming21@gmail.com>
2025-06-19 17:08:05 +0900

    useReducer의 사용

--- ch10_hooks/src/App.js
+++ ch10_hooks/src/App.js
@@ -6,10 +6,20 @@ import './App.css';
 // import Info from './ch04_useEffect/Info';
 // import Info from './ch05_useEffect/Info';
 // import Info from './ch06_useEffect/Info';
-import Info from './ch07_useEffect/Info';
+// import Info from './ch07_useEffect/Info';
+// import Info from './ch08_useEffect/Info';
+// import ParentInfo from './ch08_useEffect/ParentInfo';
+// import Counter from './ch09_useReducer/Counter';
+import Info from './ch10_useReducer/Info';
 
 function App() {
   return (
+    // <Counter />
+
+    // <Info />
+
+    // <ParentInfo />
+
     // <Counter />
 
     <Info />
