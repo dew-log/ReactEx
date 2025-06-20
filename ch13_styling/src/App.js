dc24b7dcb009e30253b6ccd701e46a2dd486e896
dew <foaming21@gmail.com>
2025-06-20 15:21:24 +0900

    Style, sass, module

---
+++ ch13_styling/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+// import SassComponent from './ex01_sass/SassComponent';
+import SassComponent from './ex02_sass/SassComponent';
+
+function App() {
+  return (
+    <SassComponent />
+  );
+}
+
+export default App;
