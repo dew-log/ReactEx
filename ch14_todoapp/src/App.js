d989f5b4be3765b1c4094cbcb9751de09c53a74e
dew <foaming21@gmail.com>
2025-06-20 17:39:29 +0900

    Todo Application

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 // import TodoApp from './ex03_TodoInsert/TodoApp';
 // import TodoApp from './ex04_TodoList/TodoApp';
 // import TodoApp from './ex05_TodoListItem/TodoApp';
-import TodoApp from './ex06_TodoList/TodoApp';
+// import TodoApp from './ex06_TodoList/TodoApp';
+// import TodoApp from './ex07_TodoInsert/TodoApp';
+import TodoApp from './ex08_onInsert/TodoApp';
 
 function App() {
   return (
--- ch14_todoapp/src/ex08_onInsert/TodoApp.jsx
+++ ch14_todoapp/src/ex08_onInsert/TodoApp.jsx
@@ -1,7 +1,7 @@
 import TodoTemplate from "./TodoTemplate";
 import TodoInsert from "./TodoInsert";
 import TodoList from "./TodoList";
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 
 const TodoApp = (props) => {
   const [todos, setTodos] = useState([
@@ -22,9 +22,24 @@ const TodoApp = (props) => {
     },
   ]);
 
+  const nextId = useRef(todos.length+1);  // 4가 초기값이 된다.
+
+  // <TodoInsert>자식 컴포넌트에 전달해서, 자식이 보내주는 text를 todos에 저장한다.
+  const onInsert = useCallback((text)=>{
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false
+    };
+    const nextTodos = [...todos];
+    nextTodos.push(todo);
+    setTodos(nextTodos);
+    nextId.current += 1;    // nextId를 1증가, useRef도 Hook이므로 Re-Rendering
+  }, [todos]);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
