bc909e69e95a17d9305a719b89be1288ac389ab7
dew <foaming21@gmail.com>
2025-06-20 16:51:37 +0900

    Todo Application

---
+++ ch14_todoapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import TodoApp from './ex01_TodoTemplate/TodoApp';
+// import TodoApp from './ex02_TodoTemplate/TodoApp';
+// import TodoApp from './ex03_TodoInsert/TodoApp';
+// import TodoApp from './ex04_TodoList/TodoApp';
+// import TodoApp from './ex05_TodoListItem/TodoApp';
+import TodoApp from './ex06_TodoList/TodoApp';
+
+function App() {
+  return (
+    <TodoApp />
+  );
+}
+
+export default App;
