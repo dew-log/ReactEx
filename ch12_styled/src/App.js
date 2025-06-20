83ed26d3b1a0a614b89bac190e43e01cf3d6b791
dew <foaming21@gmail.com>
2025-06-20 14:27:19 +0900

    스타일드 컴포넌트 진행중

---
+++ ch12_styled/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import ReactButton from './ex01_nonstyled/ReactButton';
+// import StyledButton from './ex02_styled/StyledButton';
+// import StyledButton from './ex03_styled/StyledButton';
+import StyledButton from './ex04_styled/StyledButton';
+
+function App() {
+  return (
+    // <ReactButton>안녕하세요</ReactButton>
+    // <StyledButton>감사합니다</StyledButton>
+    <StyledButton>존경합니다</StyledButton>
+  );
+}
+
+export default App;
